1b350cbea311854348ff25e94af905f11f73ecf0
ggrig <grigoryan.g@gostandup.ru>
2023-01-10 19:26:31 +0400

    01.10.23

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('search') }}" :active="request()->routeIs('search')">
+                        {{ __('Search') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
